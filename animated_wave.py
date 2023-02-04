b1e75e8379a624f833422f098ed1c021e6b52f6b
Jude Young <jude.d.l.young@icloud.com>
2023-02-04 16:50:06 +0000

    fixed animation

--- animated_wave.py
+++ animated_wave.py
@@ -6,42 +6,45 @@ from matplotlib.animation import FuncAnimation
 #init fig and axes
 fig = plt.figure()
 ax = plt.axes()
-line = ax.plot([], [])
+line, = ax.plot([], [])
 
 #init data for wave eqn
-global delta_t
-global delta_x
-global wave_speed
 delta_x = 0.5
 delta_t = 1e-2
 wave_speed = 10
 total_time = 3 #note this is the frame cache size
 x_size = 1000
+
 initial_pos = wav.init_pos_bell(x_size, 0.01, 1)
+ax.set_ylim(-max(initial_pos), max(initial_pos))
+ax.set_xlim(0, x_size)
 
 global y
 y = wav.init_model(total_time, initial_pos, [0]*len(initial_pos), delta_t)
 
-def update_wave(delta_t, delta_x, wave_speed):
+def update_wave(delta_t=delta_t, delta_x=delta_x, wave_speed=wave_speed):
     '''
     Given two previous timesteps of y(x, t), calculate the next, then roll the array back
     so when next called the function will overwrite the oldest timestep.
     '''
-    y[:, 3] = [wav.get_next_y(i, 2, y, delta_t, delta_x, wave_speed) for i in range(x_size)]
-    return np.roll(y, -1)
+    global y
+    return [wav.get_next_y(i, 1, y, delta_t, delta_x, wave_speed) for i in range(x_size)]
 
 def init():
     line.set_data([], [])
-    return line
+    return line,
 
-def animate(frame):
-    y = update_wave(delta_t, delta_x, wave_speed)
+def animate(i, x_size=x_size):
+    global y
     graphX = [range(0, x_size)]
-    graphY = y[:, 2]
+    graphY = y[:, 1]
+    print('frame: ', i, max(y[:, 1]))
     line.set_data(graphX, graphY)
-    return line
+    for k in range(20):
+        y[:, 2] = update_wave()
+        y = np.roll(y, -1, axis=1)
+    return line,
 
-anim = FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20)
+anim = FuncAnimation(fig, animate, init_func=init, frames=500, interval=10)
 
-plt.show()
+anim.save('wave_anim_loop.gif')
